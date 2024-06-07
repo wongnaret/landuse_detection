1936fa536cd26cb964b4bbd4953359260789e054
Wongnaret Khanuwan <wongnaret.kha@nectec.or.th>
2024-06-07 09:22:58 +0700

    initial commit by wongnaret

---
+++ dataset_generator/generator/conection_conf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ File name: conection_conf.py
+ Date Create: 7/6/2024 AD 08:48
+ Author: Wongnaret Khantuwan 
+ Email: wongnaet.khantuwan@nectec.or.th, wongnaret@gmail.com
+ Python Version: 3.9
+"""
+
+
+
+ODC_S2_PLATFORM = "Sentinel_2_Thailand"
+ODC_S2_PRODUCT = "s2_level2a_Thailand"
+ODC_S2_MEASUEMENT = ['coastal',
+                     'blue',
+                     'green',
+                     'red',
+                     'veg5',
+                     'veg6',
+                     'veg7',
+                     'nir',
+                     'narrow_nir',
+                     'water_vapour',
+                     'swir1',
+                     'swir2',
+                     'SCL',
+                     'WVP',
+                     'AOT',
+                     #'pixel_qa',
+                     ]
+
+ODC_S1_PLATFORM = "SENTINEL_1_AWS"
+ODC_S1_PRODUCT = "s1_gamma0_aws_general"
+ODC_S1_MEASUEMENT = ['coastal',
+                     'blue',
+                     'green',
+                     'red',
+                     'veg5',
+                     'veg6',
+                     'veg7',
+                     'nir',
+                     'narrow_nir',
+                     'water_vapour',
+                     'swir1',
+                     'swir2',
+                     'SCL',
+                     'WVP',
+                     'AOT',
+                     #'pixel_qa',
+                     ]
